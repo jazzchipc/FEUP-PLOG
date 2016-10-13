e6c01f4324d9dbc23beb7a9b2f9589ddb6302f59
José Carlos <jose__carlos17@hotmail.com>
2016-10-13 16:25:40 +0100

    Board can now draw 1 hexagon piece complete

--- board.pl
+++ board.pl
@@ -16,9 +16,12 @@ boardT([[x, x, x],
 
 translate(s8, '        ').
 translate(s7, '       ').
+translate(s6, '      ').
 translate(s5, '     ').
 translate(s3, '   ').
-translate(sl, ' ').
+translate(s2, '  ').
+translate(s1, ' ').
+translate(s0, '').
 translate(t, '___').
 translate(ub, '/').
 translate(db, '\\').
@@ -37,27 +40,27 @@ generate_empty_space(Spaces, NumberOfTimes):-
     write(EmptySpace),
     generate_empty_space(Spaces, N1).
 
-display_line_a_aux(N):-
+display_line_1_aux(N):-
     N=0,
     nl.
 
-display_line_a_aux(NumberHexagons):-
+display_line_1_aux(NumberHexagons):-
     N1 is NumberHexagons - 1,
     translate(t, A),
     translate(s7, SpaceBetweenHex),
     write(A),
     write(SpaceBetweenHex),
-    display_line_a_aux(N1).
+    display_line_1_aux(N1).
 
-display_line_a(EmptySpace, NumberEmptySpaces, NumberHexagons):-
-    generate_empty_space(EmptySpace, NumberEmptySpaces),
-    display_line_a_aux(NumberHexagons).
+display_line_1(NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(s7, NumberEmptySpaces),
+    display_line_1_aux(NumberHexagons).
 
-display_line_b_aux(N):-
+display_line_2_aux(N):-
     N = 0,
     nl.
 
-display_line_b_aux(N):-
+display_line_2_aux(N):-
     N1 is N-1,
     translate(ub, OpenHex),
     translate(s3, SpaceInsideHex),
@@ -67,14 +70,213 @@ display_line_b_aux(N):-
     write(SpaceInsideHex),
     write(CloseHex),
     write(SpaceBetweenHex),
-    display_line2(N1).
+    display_line_2_aux(N1).
 
-display_line_b(EmptySpace, NumberEmptySpaces, NumberHexagons):-
-    generate_empty_space(EmptySpace, NumberEmptySpaces),
-    display_line_b_aux(NumberHexagons).
+display_line_2(NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(s6, NumberEmptySpaces),
+    display_line_2_aux(NumberHexagons).
 
-display_board:-
-    display_line_a(s8, 2, 4),
-    display_line_b(s7, 2, 4).
+display_line_3_aux(N):-
+    N = 0,
+    nl.
 
-display:- display_board.
+display_line_3_aux(N):-
+    N1 is N-1,
+    translate(t, A),
+    translate(ub, OpenHex),
+    translate(s5, SpaceInsideHex),
+    translate(db, CloseHex),
+    write(A),
+    write(OpenHex),
+    write(SpaceInsideHex),
+    write(CloseHex),
+    display_line_3_aux(N1).
+
+display_line_3(NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(s2, NumberEmptySpaces),
+    display_line_3_aux(NumberHexagons).
+
+
+
+
+
+display_line_4_aux(N):-
+    N = 0,
+    nl.
+
+display_line_4_aux(NumberHexagons):-
+    N1 is NumberHexagons - 1,
+    translate(ub, OpenHex),
+    translate(s3, SpaceInsideHex),
+    translate(db, CloseHex),
+    translate(s5, SpaceInsideHex2),
+    write(CloseHex),
+    write(SpaceInsideHex2),
+    write(OpenHex),
+    write(SpaceInsideHex),
+    display_line_4_aux(N1).
+
+display_line_4(NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(s1, NumberEmptySpaces),
+    translate(ub, OpenHex),
+    translate(s3, SpaceInsideHex),
+    translate(db, CloseHex),
+    write(OpenHex),
+    write(SpaceInsideHex),
+    display_line_4_aux(NumberHexagons).
+
+
+
+
+
+display_line_5_aux(N):-
+    N = 0,
+    nl.
+
+display_line_5_aux(NumberHexagons):-
+    N1 is NumberHexagons - 1,
+    translate(t, A),
+    translate(ub, OpenHex),
+    translate(s5, SpaceInsideHex),
+    translate(db, CloseHex),
+    write(CloseHex),
+    write(A),
+    write(OpenHex),
+    write(SpaceInsideHex),
+    display_line_5_aux(N1).
+
+display_line_5(NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(s0, NumberEmptySpaces),
+    translate(ub, OpenHex),
+    translate(s5, SpaceInsideHex),
+    translate(db, CloseHex),
+    write(OpenHex),
+    write(SpaceInsideHex),
+    display_line_5_aux(NumberHexagons).
+
+
+
+
+
+
+display_line_6_aux(N):-
+    N = 0,
+    nl.
+
+display_line_6_aux(NumberHexagons):-
+    N1 is NumberHexagons - 1,
+    translate(ub, OpenHex),
+    translate(s3, SpaceInsideHex),
+    translate(db, CloseHex),
+    translate(s5, SpaceInsideHex2),
+    write(OpenHex),
+    write(SpaceInsideHex),
+    write(CloseHex),
+    write(SpaceInsideHex2),
+    display_line_6_aux(N1).
+
+display_line_6(NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(s0, NumberEmptySpaces),
+    translate(ub, OpenHex),
+    translate(s5, SpaceInsideHex),
+    translate(db, CloseHex),
+    translate(s5, SpaceInsideHex2),
+    write(CloseHex),
+    write(SpaceInsideHex),
+    display_line_6_aux(NumberHexagons).
+
+
+
+
+
+display_line_7_aux(N):-
+    N = 0,
+    nl.
+
+display_line_7_aux(NumberHexagons):-
+    N1 is NumberHexagons - 1,
+    translate(t, A),
+    translate(ub, OpenHex),
+    translate(s5, SpaceInsideHex),
+    translate(db, CloseHex),
+    write(A),
+    write(OpenHex),
+    write(SpaceInsideHex),
+    write(CloseHex),
+    display_line_7_aux(N1).
+
+display_line_7(NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(s1, NumberEmptySpaces),
+    translate(db, CloseHex),
+    write(CloseHex),
+    display_line_7_aux(NumberHexagons).
+
+
+
+
+
+display_line_8_aux(N):-
+    N = 0,
+    nl.
+
+display_line_8_aux(N):-
+    N1 is N-1,
+    translate(ub, OpenHex),
+    translate(s5, SpaceInsideHex),
+    translate(db, CloseHex),
+    translate(s3, SpaceInsideHex2),
+    write(CloseHex),
+    write(SpaceInsideHex),
+    write(OpenHex),
+    write(SpaceInsideHex2),
+    display_line_8_aux(N1).
+
+display_line_8(NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(s5, NumberEmptySpaces),
+    display_line_8_aux(NumberHexagons).
+
+
+
+
+
+
+display_line_9_aux(N):-
+    N = 0,
+    nl.
+
+display_line_9_aux(N):-
+    N1 is N-1,
+    translate(t, A),
+    translate(ub, OpenHex),
+    translate(db, CloseHex),
+    translate(s5, SpaceBetweenHex),
+    write(CloseHex),
+    write(A),
+    write(OpenHex),
+    write(SpaceBetweenHex),
+    display_line_9_aux(N1).
+
+display_line_9(NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(s6, NumberEmptySpaces),
+    display_line_9_aux(NumberHexagons).
+
+
+
+display_board(N):-
+    N = 0,
+    nl.
+
+display_board(N):-
+    N1 is N - 1,
+    display_line_1(1, 4),
+    display_line_2(1, 4),
+    display_line_3(1, 4),
+    display_line_4(1, 4),
+    display_line_5(1, 4),
+    display_line_6(1, 4),
+    display_line_7(1, 4),
+    display_line_8(1, 4),
+    display_line_9(1, 4),
+    display_board(N1).
+
+display:- display_board(2).
