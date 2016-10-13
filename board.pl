2a92f66291299dac0c56f63f73f3e5d2cae9a789
José Carlos <jose__carlos17@hotmail.com>
2016-10-13 11:31:01 +0100

    Display board updates

--- board.pl
+++ board.pl
@@ -1,6 +1,6 @@
 board([ [sssss, t, sssss, t],
         [ssss, ub, sl, db, sss, ub, sl, db],
-        [ss, t, ub, sss, db, t, ub, sss, db, t],
+        [ss, t, ub, sl, c, sl, db, t, ub, sl, x, sl, db, t],
         [sl, ub, sl, db, sss, ub, sl, db, sss, ub, sl, db],
         [ub, sss, db, t, ub, sss, db, t, ub, sss, db],
         [db, sss, ub, sl, db, sss, ub, sl, db, sss, ub],
@@ -8,40 +8,73 @@ board([ [sssss, t, sssss, t],
         [sss, db, sss, ub, sl, db, sss, ub],
         [ssss, db, t, ub, sss, db, t, ub]]).
 
-translate(sssss, '     ').
-translate(ssss, '    ').
-translate(sss, '   ').
-translate(ss, '  ').
+boardT([[x, x, x],
+        [x, x],
+        [x, x, x]]).
+
+% Adicionar coordenadas
+
+translate(s8, '        ').
+translate(s7, '       ').
+translate(s5, '     ').
+translate(s3, '   ').
 translate(sl, ' ').
-translate(t, '_').
+translate(t, '___').
 translate(ub, '/').
 translate(db, '\\').
 translate(nl, '\n').
+translate(c, '1').
 
-display_lineA(L1):-
-    translate(sssss, C),
-    write(C),
-    translate(t, C),
-    write(C),
-    translate(sssss, C),
-    write(C),
-    translate(t, C),
-    write(C).
+translate(x, '').
 
-display_line([]):-
+generate_empty_space(Spaces, NumberOfTimes):-
+    NumberOfTimes = 0,
     write('').
 
-display_line([E1|Es]):-
-    translate(E1, C),
-    write(C),
-    display_line(Es).
+generate_empty_space(Spaces, NumberOfTimes):-
+    N1 is NumberOfTimes - 1,
+    translate(Spaces, EmptySpace),
+    write(EmptySpace),
+    generate_empty_space(Spaces, N1).
 
-display_board([]):-
+display_line_a_aux(N):-
+    N=0,
     nl.
 
-display_board([L1|Ls]):-
-    display_line(L1),
-    nl,
-    display_board(Ls).
+display_line_a_aux(NumberHexagons):-
+    N1 is NumberHexagons - 1,
+    translate(t, A),
+    translate(s7, SpaceBetweenHex),
+    write(A),
+    write(SpaceBetweenHex),
+    display_line_a_aux(N1).
 
-display:- board(B), display_board(B).
+display_line_a(EmptySpace, NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(EmptySpace, NumberEmptySpaces),
+    display_line_a_aux(NumberHexagons).
+
+display_line_b_aux(N):-
+    N = 0,
+    nl.
+
+display_line_b_aux(N):-
+    N1 is N-1,
+    translate(ub, OpenHex),
+    translate(s3, SpaceInsideHex),
+    translate(db, CloseHex),
+    translate(s5, SpaceBetweenHex),
+    write(OpenHex),
+    write(SpaceInsideHex),
+    write(CloseHex),
+    write(SpaceBetweenHex),
+    display_line2(N1).
+
+display_line_b(EmptySpace, NumberEmptySpaces, NumberHexagons):-
+    generate_empty_space(EmptySpace, NumberEmptySpaces),
+    display_line_b_aux(NumberHexagons).
+
+display_board:-
+    display_line_a(s8, 2, 4),
+    display_line_b(s7, 2, 4).
+
+display:- display_board.
