0ea6e2b0c31a0b1b3493d159ca4127c9e92e7f56
José Carlos <jose__carlos17@hotmail.com>
2016-12-06 15:41:31 +0000

    Errors gone

--- Trabalho 2/code/yin_yang.pl
+++ Trabalho 2/code/yin_yang.pl
@@ -13,24 +13,33 @@ initial_board([
     D1, D2, D3, D4
 ]).
 
-display:-
-    initial_board(Board),
+test_board([
+    A1, A2, A3,
+    B1, B2, B3,
+    C1, C2, C3
+]).
+
+display(Board):-
+    test_board(Board),
+    length(Board, Length),
+    
     domain(Board, 0, 1),
 
     setConstrains(Board, 1),
 
     labeling([], Board).
 
-setConstrains([_, _, _, _, _], Index).
 setConstrains(Board, Index):-
-    0 =:= mod(Index, 2),
+    Index =:= 6.
+setConstrains(Board, Index):-
+    0 =:= mod(Index, 3),
     NewIndex is Index + 1,
     setConstrains(Board, NewIndex).
 setConstrains(Board, Index):-
     element(Index, Board, CurrElem),
     NextIndex is Index + 1,
     element(NextIndex, Board, ElemRight),
-    NextRow is Index + 4,
+    NextRow is Index + 3,
     element(NextRow, Board, ElemBelow),
     NextRowPlus is NextRow + 1,
     element(NextRowPlus, Board, ElemSE),
